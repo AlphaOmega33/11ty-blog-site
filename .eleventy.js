394c076f68ab9561348836363e7151146aaaef68
AlphaOmega33 <131569218+AlphaOmega33@users.noreply.github.com>
2023-06-15 07:18:24 +1000

    11tyfied-the-static-pages

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+const { datetime } = require ("luxon");
+
+module.exports = function(eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy('./src/style.css');
+    eleventyConfig.addPassthroughCopy('./src/assets');
+
+    eleventyConfig.addFilter("postDate"), (dateObj) => {
+        return datetime.FromJSDate(dateObj).toLocaleString(datetime.Date_MED);
+    };
+
+    return {
+        dir: {
+            input: "src",
+            output: "public"
+        }
+    };
+}
